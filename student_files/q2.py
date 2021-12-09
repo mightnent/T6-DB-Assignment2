327515c4c57febf45c8df5f3d09c5393ff82f71c
Mike Sun <mightnent@gmail.com>
2021-12-09 20:03:26 +0800

    Add files via upload

---
+++ student_files/q2.py
@@ -0,0 +1,43 @@
+import sys
+from pyspark.sql import SparkSession
+# you may add more import if you need to
+from pyspark.context import SparkContext
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from pyspark.sql.functions import asc,desc,col
+
+sc = SparkContext.getOrCreate()
+
+
+# don't change this line
+hdfs_nn = "172.31.6.233"
+
+spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
+# YOUR CODE GOES BELOW
+spark = SparkSession(sc)
+
+df = spark.read.option("header",True).csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+
+df = df.na.drop(subset=["Rating"])
+df = df.na.drop(subset=["Price Range"])
+
+df = df.withColumn('Rating',df['Rating'].cast("float").alias('Rating'))
+
+df.printSchema()
+
+df1 = df.groupBy('City','Price Range').agg(F.max('Rating').alias('Rating'))
+df2 = df.groupBy('City','Price Range').agg(F.min('Rating').alias('Rating'))
+
+df_u = df1.union(df2)
+
+df_join = df.join(df_u,on=["City","Price Range","Rating"])
+new_df = df_join.sort(asc("City")).dropDuplicates(subset=["City","Price Range","Rating"])
+
+new_df.show()
+
+new_df.write.csv("hdfs://%s:9000/assignment2/output/question2/" % (hdfs_nn), header=True)
+
+
+
+
+
